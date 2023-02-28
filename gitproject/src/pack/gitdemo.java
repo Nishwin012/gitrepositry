aed5eea9bee98edb72dc9acba35ac50ab5e2db16
nishwin <xyz@gmail.com>
2023-02-28 10:17:19 +0530

    updated

---
+++ gitproject/src/pack/gitdemo.java
@@ -0,0 +1,18 @@
+package pack;
+
+public class gitdemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome");
+		
+		
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+
+	}
+
+}
